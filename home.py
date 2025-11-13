d47ce251b674309479f961d04d46005e45d9bb81
Alaa <alaa168000@gmail.com>
2025-11-13 04:16:59 +0300

    Add number sign check

--- home.py
+++ home.py
@@ -21,3 +21,10 @@ elif num1 < num2:
     print(num1, "is less than", num2)
 else:
     print(num1, "is equal to", num2)
+
+if num1 > 0:
+    print(num1, "is positive")
+elif num1 < 0:
+    print(num1, "is negative")
+else:
+    print(num1, "is zero")
