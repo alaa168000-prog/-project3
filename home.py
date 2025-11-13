f9aac3077bace39bf3586994ea70db1d079df4d9
Alaa <alaa168000@gmail.com>
2025-11-13 04:23:53 +0300

    Add repeat calculation feature

--- home.py
+++ home.py
@@ -1,30 +1,36 @@
-num1 =float(input("enter first number"))
-operator =input("enter operator")
-num2 =float(input("enter second number"))
+while True:
+    num1 =float(input("enter first number"))
+    operator =input("enter operator")
+    num2 =float(input("enter second number"))
 
-if operator == "+":
-    print(num1 + num2)
-elif operator == "-":
-    print(num1 - num2)
-elif operator == "*":
-    print(num1 * num2)
-elif operator == "/":
-    print(num1 / num2)
-elif operator == "**":
-    print(num1 ** num2)
-else:
-    print("invalid operator")
+    if operator == "+":
+        print(num1 + num2)
+    elif operator == "-":
+        print(num1 - num2)
+    elif operator == "*":
+        print(num1 * num2)
+    elif operator == "/":
+        print(num1 / num2)
+    elif operator == "**":
+        print(num1 ** num2)
+    else:
+        print("invalid operator")
 
-if num1 > num2:
-    print(num1, "is greater than", num2)
-elif num1 < num2:
-    print(num1, "is less than", num2)
-else:
-    print(num1, "is equal to", num2)
+    if num1 > num2:
+        print(num1, "is greater than", num2)
+    elif num1 < num2:
+        print(num1, "is less than", num2)
+    else:
+        print(num1, "is equal to", num2)
 
-if num1 > 0:
-    print(num1, "is positive")
-elif num1 < 0:
-    print(num1, "is negative")
-else:
-    print(num1, "is zero")
+    if num1 > 0:
+        print(num1, "is positive")
+    elif num1 < 0:
+        print(num1, "is negative")
+    else:
+        print(num1, "is zero")
+
+    again = input("Do you want to calculate again? (y/n): ")
+    if again != "y":
+        print("Goodbye!")
+        break
