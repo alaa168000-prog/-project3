c79273d006b12f70bcb7dfad3a458c9c6e80f994
Alaa <alaa168000@gmail.com>
2025-11-13 04:11:53 +0300

    Add comparison between two numbers

--- home.py
+++ home.py
@@ -14,3 +14,10 @@ elif operator == "**":
     print(num1 ** num2)
 else:
     print("invalid operator")
+
+if num1 > num2:
+    print(num1, "is greater than", num2)
+elif num1 < num2:
+    print(num1, "is less than", num2)
+else:
+    print(num1, "is equal to", num2)
